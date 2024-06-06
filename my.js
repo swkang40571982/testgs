ab47596a14915f46aa67703035f568413bb99475
swkang40571982 <32179018+swkang40571982@users.noreply.github.com>
2024-06-06 18:41:33 +0900

    Update my.js

--- my.js
+++ my.js
@@ -1,6 +1,6 @@
 function testGS() {
 
-  const url="https://script.google.com/macros/s/AKfycbwVhbvPoLe3uOTv0WaVu8zTQ_esLZIcJWHmBRYznY_YLywRWVyJPGWRitNWtAVkRo8Q0A/exec"
+  const url=""
 
   fetch(url)
     .then(d => d.json())
