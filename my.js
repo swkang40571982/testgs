b9590ca06821702a949a5ddcec75e0af16aa6040
swkang40571982 <32179018+swkang40571982@users.noreply.github.com>
2024-06-06 19:44:55 +0900

    Update my.js

--- my.js
+++ my.js
@@ -1,6 +1,27 @@
+function addGS() {
+
+  const url="https://script.google.com/macros/s/AKfycbwOH3UM0Qi9CpOaZMuePqIwosDiyDQ0SGxvCXcUp3oFI6NrJ4F5HupgRGYzHA_jAuQL-A/exec"
+  const data = { name: "John" }
+  fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    // mode: "cors", // no-cors, *cors, same-origin
+    mode: "no-cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    // credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  })
+
+}
+
 function testGS() {
 
-  const url=""
+  const url="https://script.google.com/macros/s/AKfycbwOH3UM0Qi9CpOaZMuePqIwosDiyDQ0SGxvCXcUp3oFI6NrJ4F5HupgRGYzHA_jAuQL-A/exec"
 
   fetch(url)
     .then(d => d.json())
@@ -9,5 +30,7 @@ function testGS() {
     })
 
 }
+
+document.getElementById("btn2").addEventListener("click", addGS)
 
 document.getElementById("btn").addEventListener("click", testGS)
